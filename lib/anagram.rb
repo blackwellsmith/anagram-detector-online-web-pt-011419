94bdec2b61b9d35510c076db19d8d8a79a9ea450
blackwell <blackwellsmithiv@gmail.com>
2019-02-07 12:46:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,7 @@
 require "pry"
 class Anagram 
   attr_accessor :word
+<<<<<<< HEAD
   
   def initialize(word)
     @word = word
@@ -13,4 +14,17 @@ class Anagram
     end
   end
   
+=======
+  array = []
+  def initialize(word)
+    @word = word
+  end
+  def self.match(words_array) #match detects no match and anagrams 
+    words_array.select do |a|
+      (@word.split("").sort) == (a.split("").sort) 
+      array << a
+    end
+  end
+  array
+>>>>>>> 53d8a8f7e282ce44e8c7be816ef35debb2a36f90
 end
